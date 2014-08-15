aadbe997caab82540f3272376e7e08cd3107758b
Mat Wall <mat.wall@digital.cabinet-office.gov.uk>
2014-08-15 16:30:42 +0100

    Cleaned up some pep-8 compliance

--- decision/server.py
+++ decision/server.py
@@ -1,14 +1,14 @@
 from flask import request
 from decision import app
 from decision_service import perform_action
-import json
+
 
 @app.route('/')
 def index():
     return "OK"
 
+
 @app.route('/decisions', methods=['POST'])
 def decision():
     if request.method == 'POST':
-        # validate json
         return perform_action(request.json, app.config['CASEWORK_URL'], app.config['CHECK_URL'])
--- tests/test_scenarios.py
+++ tests/test_scenarios.py
@@ -4,20 +4,16 @@ from decision.scenarios import *
 class CheckChangeNameMarriage(unittest.TestCase):
 
     def test_given_data_with_uppercase_gb_returns_true(self):
-        data = {"iso-country-code": "GB"}
-        self.assertTrue(check_change_name_marriage(data))
+        self.assertTrue(check_change_name_marriage({"iso-country-code": "GB"}))
 
     def test_given_data_with_lowercase_gb_returns_false(self):
-        data = {"iso-country-code": "gb"}
-        self.assertFalse(check_change_name_marriage(data))
+        self.assertFalse(check_change_name_marriage({"iso-country-code": "gb"}))
 
     def test_given_data_with_aq_returns_false(self):
-        data = {"iso-country-code": "AQ"}
-        self.assertFalse(check_change_name_marriage(data))
+        self.assertFalse(check_change_name_marriage({"iso-country-code": "AQ"}))
 
     def test_given_data_with_none_returns_false(self):
-        data = {"iso-country-code": None}
-        self.assertFalse(check_change_name_marriage(data))
+        self.assertFalse(check_change_name_marriage({"iso-country-code": None}))
 
     def test_given_no_data_return_false(self):
         self.assertRaises(AttributeError, check_change_name_marriage, None)
