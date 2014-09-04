80334f26d6730fbd4d1c433bb118650a9081bb9d
Mat Wall <mat.wall@digital.cabinet-office.gov.uk>
2014-09-04 15:42:44 +0000

    Development environment config is already set by dev-env-functions

--- environment.sh
+++ environment.sh
@@ -1,3 +1,2 @@
-export SETTINGS='config.DevelopmentConfig'
 export CASEWORK_URL='http://casework-frontend.landregistry.local/casework'
 export CHECK_URL='http://casework-frontend.landregistry.local/checks'
